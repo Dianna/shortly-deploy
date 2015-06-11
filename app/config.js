5525b65a27ddb5aaa93a497dc6f163d02d2dce63
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 12:24:12 -0700

    running mongo on test

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 // mongoose.connect('mongodb:your_database_user:password@127.0.0.1/db')
-mongoose.connect('mongodb://localhost/xdb');
+mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 
